58126eec6b11424447339733991810c69ca5c013
PAV <pavdeepgill@gmail.com>
2023-05-18 19:56:49 -0700

    More markers

--- programs/GoogleMapsApp/main.js
+++ programs/GoogleMapsApp/main.js
@@ -9,7 +9,7 @@ async function initMap()
 
   map = new Map(document.getElementById("map"), { // create map
     center: { lat: 49.2827, lng: -123.1207 }, // center it at Vancouver: 49.2827° N, 123.1207° W (West = negative, South = negative)
-    zoom: 10,
+    zoom: 9.8,
     mapId: '1e91fe057ddd1491', 
   });
   
@@ -27,22 +27,66 @@ const markers = [ // we will add a marker at each of the following location arra
   "Vancouver", // Location Array 1's [Element 0]
   49.2827, // Location Array 1's [Element 1]
   -123.1207, // Location Array 1's [Element 2]
-  (20,20) // Location Array 1's [Element 3]
+  25,
+  37 // Location Array 1's [Element 3]
   ],
 
   [ // Location Array 2
   "Burnaby", // same element setup as previous Location Array
   49.2488,
   -122.9805,
-  (3,3)
+  23,
+  30
   ],
 
   [ // Location Array 3
   "Surrey",
   49.1913,
   -122.8490,
-  (3,3)
+  23,
+  30
   ],
+
+  [ // Location Array 4
+  "Richmond",
+  49.1666,
+  -123.1336,
+  23,
+  30
+  ],
+
+  [ // Location Array 5
+  "Coquitlam",
+  49.2838,
+  -122.7932,
+  23,
+  30
+  ],
+
+  [ // Location Array 6
+  "Abbotsford",
+  49.0504,
+  -122.3045,
+  23,
+  30
+  ],
+
+  [ // Location Array 7
+  "Langley",
+  49.1042,
+  -122.6604,
+  23,
+  30
+  ],
+
+  [ // Location Array 8
+  "White Rock",
+  49.0253,
+  -122.8030,
+  23,
+  30
+  ],
+
 ];
 
 
@@ -77,13 +121,29 @@ for (let i = 0; i < markers.length; i++)
   const currMarker = markers[i];
 
   // Create a Google Maps Marker
- new google.maps.Marker({
+ const marker = new google.maps.Marker({
     position: {lat: currMarker[1], lng: currMarker[2]},
     map,
-    icon: image, // scaledSize: new google.maps.Size(currMarker[3])
+    icon: 
+    {
+      url: image.url,
+      // This marker is 20 pixels wide by 32 pixels high.
+      size: new google.maps.Size(currMarker[3], currMarker[4])
+    }, // scaledSize: new google.maps.Size(currMarker[3])
     shape: shape,
     title: currMarker[0], // Title "Name" of Location
     zIndex: 2,
+  });
+
+  // Info Window Popup Feature
+  const infowindow = new google.maps.InfoWindow({
+    content: 
+    "<div>" + currMarker[0] + "<br>Latitude: " + currMarker[1] + "<br>Longitude: " + currMarker[2] + "</div>",
+  });
+
+  // Then lets have our event listener attached to that Google Maps Marker
+  marker.addListener("click", () => {
+    infowindow.open(map, marker);
   });
 
   } // end of for loop
@@ -93,14 +153,6 @@ window.initMap() = initMap();
 
 /* 
 
-  // Info Window Popup Feature
-  const infowindow = new google.maps.InfoWindow({
-    content: currMarker[0], // Title
-  });
 
-  // Then lets have our event listener attached to that Google Maps Marker
-  marker.addListener("click", () => {
-    infowindow.open(map, marker);
-  });
 
 */
